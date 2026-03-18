fa6cb921d665c28cefa874356c9fc989cbc97d89
okolodk <grisha.zhivot@bk.ru>
2026-03-18 16:12:56 +0300

    Step 4: Add words data and game logic

--- app/src/main/java/com/ryckluk/unscramblegame/ui/theme/ui_model/GameViewModel.kt
+++ app/src/main/java/com/ryckluk/unscramblegame/ui/theme/ui_model/GameViewModel.kt
@@ -2,18 +2,45 @@ package com.ryckluk.unscramblegame.ui.theme.ui_model
 
 import androidx.lifecycle.ViewModel
 import com.ryckluk.unscramblegame.data.GameUiState
+import com.ryckluk.unscramblegame.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class GameViewModel : ViewModel(){
+class GameViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(GameUiState())
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWords = word.toCharArray()
+        tempWords.shuffle()
+        while (String(tempWords) == word) {
+            tempWords.shuffle()
+        }
+        return String(tempWords)
+    }
+
+    private fun pickRandomWordAndShuffle(): String{
+        currentWord = allWords.random()
+        while (usedWords.contains(currentWord)){
+            currentWord = allWords.random()
+        }
+        usedWords.add(currentWord)
+        return shuffleCurrentWord(currentWord)
+    }
 
     init {
         resetGame()
     }
-    fun resetGame(){
+
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(
+            currentScrambledWord = pickRandomWordAndShuffle()
+        )
 
     }
 }
